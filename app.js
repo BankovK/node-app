7a39fee1958661ccae48938b96d73fd005910deb
KBankou <KBankou@gomel.iba.by>
2021-11-22 21:44:35 +0300

    sessions

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var session = require('express-session');
+const bodyParser = require('body-parser');
 var moment = require('moment');
+const router = express.Router();
 var app = express();
 
 const mysql = require("mysql");
+
+var sess;
+
+app.use(session({secret: 'test1', saveUninitialized: true, resave: true}));
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
 
 const pool = mysql.createPool({
  connectionLimit: 100,
@@ -14,11 +23,24 @@ const pool = mysql.createPool({
  debug: true,
 });
 
-app.get('/time', function(req, res) {
-  res.send('Time is:' + moment(Date.now()).format('lll'));
+router.post('/login', (req, res) => {
+  sess = req.session;
+  res.end('login successful');
 });
 
-app.get('/data', function(req, res) {
+router.get('/time', (req, res) => {
+  if(sess) {
+    res.send('Time is:' + moment(Date.now()).format('lll'));
+  } else {
+    res.send('Please login');
+  }
+});
+
+router.get('/data', (req, res) => {
+  if(!sess) {
+    res.send('Please login');
+    return;
+  }
   pool.query("SELECT name from `kfc-db`.products LIMIT 10", (err, rows) => {
     if (err) {
       console.log("Error occurred during the connection.");
@@ -28,4 +50,18 @@ app.get('/data', function(req, res) {
   });
 });
 
-var server = app.listen(3000,function() {});
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if(err) {
+        return console.log(err);
+    }
+    sess = null;
+    res.end('You have logged out.');
+  });
+});
+
+app.use('/', router);
+
+var server = app.listen(process.env.PORT || 3000,() => {
+  console.log(`App Started on PORT ${process.env.PORT || 3000}`);
+});
